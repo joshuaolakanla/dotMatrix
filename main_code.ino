8eee1048738dcd4617feb57f39d280d74e052d2f
Joshua Olakanla <34883704+joshuaolakanla@users.noreply.github.com>
2023-02-13 16:40:57 +0100

    Update main_code.ino

--- main_code.ino
+++ main_code.ino
@@ -6,9 +6,9 @@
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 8
 
-#define CLK_PIN   9
-#define DATA_PIN  11
-#define CS_PIN    10
+#define CLK_PIN   5
+#define DATA_PIN  0
+#define CS_PIN    4
 
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
@@ -19,13 +19,14 @@ void setup(void)
   P.setZone(1, 4, 7);
   
   
-      P.setFont(1, BigFontLower);
-      P.setFont(0, BigFontUpper);
-      P.displayZoneText(0, "612", PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-      P.displayZoneText(1, "612", PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
+      
 }
 
 void loop(void)
 {
+  P.setFont(1, BigFontLower);
+  P.setFont(0, BigFontUpper);
+  P.displayZoneText(0, "612", PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
+  P.displayZoneText(1, "612", PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
   P.displayAnimate();
     }
