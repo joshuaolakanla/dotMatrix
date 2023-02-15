8795a496503fda7565563ab840686adc953935b7
Joshua Olakanla <34883704+joshuaolakanla@users.noreply.github.com>
2023-02-15 10:31:41 +0100

    Update and rename feb13.ino to feb15.ino

--- feb15.ino
+++ feb15.ino
@@ -1,4 +1,6 @@
+//Code by Joshua Olakanla Ayoola (jossytech)
 //192.168.4.1/ nodemcu address
+//http://192.168.4.1/421,428,89,120,987,977,301
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
@@ -9,27 +11,35 @@
 #include <EEPROM.h>
 #include "Font_Data.h"
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 48
-#define CLK_PIN   4
-#define DATA_PIN  12
-#define CS_PIN    16
-/*
-#define CLK_PIN2   5
-#define DATA_PIN2  0
-#define CS_PIN2    2 */
-String a,b,c,d,e,f;
-String aD,bD,cD,dD,eD,fD;
+#define MAX_DEVICES 24
+#define CLK_PIN   4 //D2
+#define DATA_PIN  12 //D6
+#define CS_PIN    16 //D0
+
+#define CLK_PIN2   5 //D1
+#define DATA_PIN2  0 //D3
+#define CS_PIN2    14 //D5
+
+#define CLK_PIN3   2 //D4
+#define DATA_PIN3  13 //D7
+#define CS_PIN3    15 //D8
+
+
+String a,b,c,d,e,f,g;
+String aD,bD,cD,dD,eD,fD,gD;
 char aOut[6];
 char bOut[6];
 char cOut[6];
 char dOut[6];
 char eOut[6];
 char fOut[6];
+char gOut[30];
 
 const char WiFiPassword[] = "";
 const char AP_NameChar[] = "Hymn" ;
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-//MD_Parola P1 = MD_Parola(HARDWARE_TYPE, DATA_PIN2, CLK_PIN2, CS_PIN2, 16);
+MD_Parola P1 = MD_Parola(HARDWARE_TYPE, DATA_PIN2, CLK_PIN2, CS_PIN2, 8);
+MD_Parola P2 = MD_Parola(HARDWARE_TYPE, DATA_PIN3, CLK_PIN3, CS_PIN3, MAX_DEVICES);
 WiFiServer server(80);
 String request = "";
 String proRequest;
@@ -110,13 +120,25 @@ String fR(int add6){
   
 }
 
+String gR(int add7){
+  int gLen = EEPROM.read(add7);
+  char gData[gLen + 1];
+  for (int j=0; j<gLen; j++){
+    gData[j]=EEPROM.read(add7 + 1 +j);
+  }
+  gData[gLen] = '\0';
+  return String(gData);
+  
+}
+
 void setup(void)
 {
   Serial.begin(115200);
   EEPROM.begin(512);
   
   P.begin(12);
-  //P1.begin(2);
+  P1.begin(2);
+  P2.begin(12);
   
   P.setZone(0, 0, 3);
   P.setZone(1, 4, 7);
@@ -124,16 +146,27 @@ void setup(void)
   P.setZone(3, 12, 15);
   P.setZone(4, 16, 19);
   P.setZone(5, 20, 23);
-  P.setZone(6, 24, 27);
-  P.setZone(7, 28, 31);
-  P.setZone(8, 32, 35);
-  P.setZone(9, 36, 39);
-  P.setZone(10, 40, 43);
-  P.setZone(11, 44, 47);
+  //P.setZone(6, 24, 27);
+  //P.setZone(7, 28, 31);
+  //P.setZone(8, 32, 35);
+  //P.setZone(9, 36, 39);
+  //P.setZone(10, 40, 43);
+  //P.setZone(11, 44, 47);
   
-  //P1.setZone(0, 0, 3);
-  //P1.setZone(1, 4, 7);
-  P.setIntensity(0);
+  P1.setZone(0, 0, 3);
+  P1.setZone(1, 4, 7);
+
+  P2.setZone(0, 0, 3);
+  P2.setZone(1, 4, 7);
+  P2.setZone(2, 8, 11);
+  P2.setZone(3, 12, 15);
+  P2.setZone(4, 16, 19);
+  P2.setZone(5, 20, 23);
+
+
+  P.setIntensity(8);
+  P1.setIntensity(8);
+  P2.setIntensity(8);
   
   P.setFont(0, BigFontUpper);
   P.setFont(1, BigFontLower);
@@ -141,15 +174,22 @@ void setup(void)
   P.setFont(3, BigFontLower);
   P.setFont(4, BigFontUpper);
   P.setFont(5, BigFontLower);
-  P.setFont(6, BigFontUpper);
+  /*P.setFont(6, BigFontUpper);
   P.setFont(7, BigFontLower);
   P.setFont(8, BigFontUpper);
   P.setFont(9, BigFontLower);
   P.setFont(10, BigFontUpper);
-  P.setFont(11, BigFontLower); 
+  P.setFont(11, BigFontLower); */
 
-  //P1.setFont(0, BigFontUpper);
-  //P1.setFont(1, BigFontLower);
+  P1.setFont(0, BigFontUpper);
+  P1.setFont(1, BigFontLower);
+
+  P2.setFont(0, BigFontUpper);
+  P2.setFont(1, BigFontLower);
+  P2.setFont(2, BigFontUpper);
+  P2.setFont(3, BigFontLower);
+  P2.setFont(4, BigFontUpper);
+  P2.setFont(5, BigFontLower);
 
 
   WiFi.disconnect();
@@ -162,6 +202,7 @@ void setup(void)
   dD = dR(30);
   eD = eR(40);
   fD = fR(50);
+  gD = gR(60);
 
 
   aD.toCharArray(aOut, 6);
@@ -170,9 +211,14 @@ void setup(void)
   dD.toCharArray(dOut, 6);
   eD.toCharArray(eOut, 6);
   fD.toCharArray(fOut, 6);
+  gD.toCharArray(gOut, 30);
 
   Serial.print("generated data...");
-  Serial.println(aOut);
+  Serial.println(gOut);
+
+
+  P1.displayZoneText(0, gOut, PA_CENTER, 60, 20, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  P1.displayZoneText(1, gOut, PA_CENTER, 60, 20, PA_SCROLL_LEFT, PA_SCROLL_LEFT); 
 
       
 }
@@ -186,6 +232,7 @@ void display(){
   dD = dR(30);
   eD = eR(40);
   fD = fR(50);
+  gD = gR(60);
 
 
   aD.toCharArray(aOut, 6);
@@ -194,19 +241,28 @@ void display(){
   dD.toCharArray(dOut, 6);
   eD.toCharArray(eOut, 6);
   fD.toCharArray(fOut, 6);
+  gD.toCharArray(gOut, 30);
 
-  P.displayZoneText(0, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(1, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(2, bOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(3, bOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(4, cOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(5, cOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(6, dOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(7, dOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(8, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(9, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(10, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT);
-  P.displayZoneText(11, aOut, PA_CENTER, 30, 30000, PA_PRINT, PA_PRINT); 
+  P.displayZoneText(0, aOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(1, aOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(2, bOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(3, bOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(4, cOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(5, cOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  /*P.displayZoneText(6, dOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(7, dOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(8, eOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(9, eOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(10, fOut, PA_CENTER, 30,300, PA_PRINT, PA_PRINT);
+  P.displayZoneText(11, fOut, PA_CENTER, 30,300, PA_PRINT, PA_PRINT); */
+  P2.displayZoneText(0, dOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P2.displayZoneText(1, dOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P2.displayZoneText(2, eOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P2.displayZoneText(3, eOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P2.displayZoneText(4, fOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+  P2.displayZoneText(5, fOut, PA_CENTER, 30, 300, PA_PRINT, PA_PRINT);
+
+  
 
 
 }
@@ -220,6 +276,7 @@ void loop(void)
   dD = dR(30);
   eD = eR(40);
   fD = fR(50);
+  gD = gR(60);
 
 
   aD.toCharArray(aOut, 6);
@@ -228,10 +285,19 @@ void loop(void)
   dD.toCharArray(dOut, 6);
   eD.toCharArray(eOut, 6);
   fD.toCharArray(fOut, 6);
+  gD.toCharArray(gOut, 30);
 
   display();
   P.displayAnimate();
-  //P1.displayAnimate();
+  P1.displayAnimate();
+  P2.displayAnimate();
+
+  if (P1.getZoneStatus(0) && P1.getZoneStatus(1)){
+    P1.setTextEffect(0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+    P1.setTextEffect(1, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+    P1.displayReset(0);
+    P1.displayReset(1);
+  }
 
 
   WiFiClient client = server.available();
@@ -261,6 +327,7 @@ void loop(void)
   d = strings[3];
   e = strings[4];
   f = strings[5];
+  g = strings[6];
   
   aW(0, a);
   bW(10, b);
@@ -268,6 +335,8 @@ void loop(void)
   dW(30, d);
   eW(40, e);
   fW(50, f);
+  gW(60, g);
+
 
   aD = aR(0);
   bD = bR(10);
@@ -275,6 +344,7 @@ void loop(void)
   dD = dR(30);
   eD = eR(40);
   fD = fR(50);
+  gD = gR(60);
 
 
   aD.toCharArray(aOut, 6);
@@ -283,6 +353,7 @@ void loop(void)
   dD.toCharArray(dOut, 6);
   eD.toCharArray(eOut, 6);
   fD.toCharArray(fOut, 6);
+  gD.toCharArray(gOut, 30);
 
 
   
@@ -357,6 +428,16 @@ void fW(int add6, const String &fS){
   EEPROM.put(add6, fLen);
   for (int i=0; i<fLen; i++){
     EEPROM.put(add6+1+i, fS[i]);
+    EEPROM.commit();
+  }
+  
+}
+
+void gW(int add7, const String &gS){
+  byte gLen = gS.length();
+  EEPROM.put(add7, gLen);
+  for (int i=0; i<gLen; i++){
+    EEPROM.put(add7+1+i, gS[i]);
     EEPROM.commit();
   }
   
